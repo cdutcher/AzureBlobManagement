1241b8e8dced4d48490c9e34197ee2850f228f8b
Cole Dutcher <cole@jet.com>
2015-01-13 10:59:03 -0500

    first commit

---
+++ AzureStorageSandbox/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure;
+using System.IO;
+
+namespace AzureStorageSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=jetcdn;AccountKey=accountkey");
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("yourcontainer");
+            
+            //CloudBlockBlob blockBlob = container.GetBlockBlobReference("025700702511.JPG");
+
+            foreach (var blob in container.ListBlobs().OfType<CloudBlockBlob>())
+            {
+                //if (Path.GetExtension(blob.Uri.AbsoluteUri) == ".GIF")
+                //{
+                //    blob.Properties.ContentType = "image/gif";
+                //    Console.WriteLine(blob.Name);
+                //}
+                
+                //blob.SetProperties();
+            }
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+    }
+}
